8b1d2535ae48a9b6fe167782bc72169fa6ae21ec
basuabhirup <basu.abhirup@yahoo.com>
2021-07-10 13:52:19 +0530

    Added drum sounds for corresponding button clicks & keyboard presses.

--- index.js
+++ index.js
@@ -1,45 +1,56 @@
+// Detecting Click on the Drum Buttons
 for (var i = 0; i<document.querySelectorAll(".drum").length; i++) {
   document.querySelectorAll(".drum")[i].addEventListener("click", function () {
-
-    switch (this.innerHTML) {
-      case "w":
-          var audio = new Audio("./sounds/tom-1.mp3");
-          audio.play();
-        break;
-
-      case "a":
-          var audio = new Audio("./sounds/tom-2.mp3");
-          audio.play();
-        break;
-
-      case "s":
-          var audio = new Audio("./sounds/tom-3.mp3");
-          audio.play();
-        break;
-
-      case "d":
-          var audio = new Audio("./sounds/tom-4.mp3");
-          audio.play();
-        break;
-
-      case "j":
-          var audio = new Audio("./sounds/snare.mp3");
-          audio.play();
-        break;
-
-      case "k":
-          var audio = new Audio("./sounds/crash.mp3");
-          audio.play();
-        break;
-
-      case "l":
-          var audio = new Audio("./sounds/kick-bass.mp3");
-          audio.play();
-        break;
-
-
-      default: console.log(this.innerHTML);
-
-    }
+    drumSound(this.innerHTML);
     });
+}
+
+// Detecting Keyboard press
+document.addEventListener("keydown", function(){
+  drumSound(event.key);
+});
+
+
+// A function to switch sounds against proper button clicks / keyboard presses
+function drumSound (key) {
+  switch (key) {
+    case "w":
+        var audio = new Audio("./sounds/tom-1.mp3");
+        audio.play();
+      break;
+
+    case "a":
+        var audio = new Audio("./sounds/tom-2.mp3");
+        audio.play();
+      break;
+
+    case "s":
+        var audio = new Audio("./sounds/tom-3.mp3");
+        audio.play();
+      break;
+
+    case "d":
+        var audio = new Audio("./sounds/tom-4.mp3");
+        audio.play();
+      break;
+
+    case "j":
+        var audio = new Audio("./sounds/snare.mp3");
+        audio.play();
+      break;
+
+    case "k":
+        var audio = new Audio("./sounds/crash.mp3");
+        audio.play();
+      break;
+
+    case "l":
+        var audio = new Audio("./sounds/kick-bass.mp3");
+        audio.play();
+      break;
+
+
+    default: console.log(key);
+
+  }
 }
